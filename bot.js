556f3fd7a35dfc5aecf6bf8afa3d43b2f0149293
Martin Cerdeira <mcerdeira@provincianet.com.ar>
2021-02-17 12:53:07 -0300

    add items

--- bot.js
+++ bot.js
@@ -3,16 +3,18 @@ const Discord = require('discord.js');
 require("dotenv").config();
 const client = new Discord.Client();
 const url = `https://discord.com/oauth2/authorize?client_id=${process.env.CLIENTID}&scope=bot`;
-const topics = JSON.parse(fs.readFileSync("./topics.json", "utf8"));
+const topicslist = JSON.parse(fs.readFileSync("./topics.json", "utf8"));
+const topics = topicslist.topics;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    console.log(topics);
   });
 
 client.on('message', msg => {
     if (msg.mentions.has(client.user.id)) {
         let tokens = msg.content.split(" ");
-        if(tokens.length == 2){
+        if(tokens.length >= 2){
             switch(tokens[1]){
                 case "idea":
                     let idx = Math.floor(Math.random() * topics.length);
@@ -22,14 +24,21 @@ client.on('message', msg => {
                     msg.reply(topics);
                     break;
                 case "help":
-                    msg.reply(`Los comandos son: idea, list y help! Para Invitarme: ${url}`);
+                    msg.reply(`Los comandos son: idea, list, add y help! Para Invitarme: ${url}`);
                     break;
                 case "add":
                     if(tokens.length >= 3){
                         let topic = tokens.slice(2, tokens.length).join(" ");
-
+                        topicslist.topics.push(topic);
+                        fs.writeFile("./topics.json", JSON.stringify(topicslist), (err) => {
+                            if (err) {
+                                console.error(err);
+                            } else {
+                                msg.reply("Listorti!");
+                            }
+                          });
                     } else {
-                        msg.reply("What?");
+                        msg.reply("What???");
                     }
                     break;
                 default:
@@ -37,7 +46,7 @@ client.on('message', msg => {
                     break;
             }
         } else {
-            msg.reply("What?");
+            msg.reply("What??");
         }
     }
 });
