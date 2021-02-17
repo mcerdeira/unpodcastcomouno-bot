7243996a5375687be2f2cc6b6c0eafe370358349
Martin Cerdeira <mcerdeira@provincianet.com.ar>
2021-02-17 12:28:58 -0300

    store topics in a json

--- bot.js
+++ bot.js
@@ -1,25 +1,9 @@
+const fs = require("fs");
 const Discord = require('discord.js');
 require("dotenv").config();
 const client = new Discord.Client();
 const url = `https://discord.com/oauth2/authorize?client_id=${process.env.CLIENTID}&scope=bot`;
-const topics = [
-    "Quests de mierda",
-    "Juegos de pelea",
-    "Juegos de diskettes",
-    "El juego que creés que ninguno de los otros de nosotros conoce",
-    "Juegos XXX",
-    "Mitos urbanos de video juegos",
-    "Juegos abandonados por frustración",
-    "Un item que recuerdes",
-    "Arcades de ayer y hoy. Tenemos para charlar de los que jugabamos de pibes y varios fuimos al sega club de Akihabara como para contar de lo que hay hoy alla.",
-    "Códigos de protección en los juegos pirateados",
-    "mi primera pc",
-    "Mirc",
-    "'Debo haber parecido un idiota!': trucos/habilidades/secretos que descubrimos demasiado tarde",
-    "Virus o malware que hayan sufrido",
-    "Juegos que todos deberían jugar",
-    "Japanofilia - Cosas que nos gusten o nos llamen la atención de japón, o anécdotas de viaje",
-]
+const topics = JSON.parse(fs.readFileSync("./topics.json", "utf8"));
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -39,6 +23,14 @@ client.on('message', msg => {
                     break;
                 case "help":
                     msg.reply(`Los comandos son: idea, list y help! Para Invitarme: ${url}`);
+                    break;
+                case "add":
+                    if(tokens.length >= 3){
+                        let topic = tokens.slice(2, tokens.length).join(" ");
+
+                    } else {
+                        msg.reply("What?");
+                    }
                     break;
                 default:
                     msg.reply("What?");
